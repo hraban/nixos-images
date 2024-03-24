293be58b75cd4909f35f7655b1ff745347e5b0b0
Hraban Luyat <hraban@0brg.net>
2024-03-24 16:53:53 -0400

    feat: always enable flakes
    Notably this allows the use of builtins.getFlake in userdata.

--- digitalocean-config.nix
+++ digitalocean-config.nix
@@ -1,4 +1,5 @@
 { ... }: {
+  nix.settings.experimental-features = "nix-command flakes";
   swapDevices = [ {
     device = "/var/lib/swapfile";
     size = 4 * 1024; # 4 GiB swap to support evaluating nixpgks.
