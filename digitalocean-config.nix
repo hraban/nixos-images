99ca70d68ec9d8fa6312f885324b89bd118c8154
Hraban Luyat <hraban@0brg.net>
2024-07-10 00:10:19 -0400

    fix: 3 GiB swap instead of 4.
    Works better on the 10 GiB droplets.

--- digitalocean-config.nix
+++ digitalocean-config.nix
@@ -2,6 +2,11 @@
   nix.settings.experimental-features = "nix-command flakes";
   swapDevices = [ {
     device = "/var/lib/swapfile";
-    size = 4 * 1024; # 4 GiB swap to support evaluating nixpgks.
+    # Swap to evaluate nixpkgs even on the smallest droplets (10 GiB disk with
+    # 512 MiB RAM).  3 GiB seems to hit the sweet spot between evaluating
+    # nixpkgs and not running out of disk space at least until the next phase in
+    # the bootstrap process.  Of course this can overridden / disabled in a
+    # do-userdata.nix.
+    size = 3 * 1024;
   } ];
 }
